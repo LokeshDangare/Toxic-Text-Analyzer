90e24cb9eb2fe8cfa584058f532fd31c761da2c6
Lokesh Dangare <lokeshdangare05@gmail.com>
2024-09-12 13:39:00 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="toxic-text-analyzer",
+    version="0.0.1",
+    author="Lokesh Dangare",
+    author_email="lokeshdangare05@gmail.com",
+    packages=find_packages(),
+    install_requires= [],
+)
